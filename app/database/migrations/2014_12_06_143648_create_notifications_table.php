8cf9b863aacff9f190339079b873342121856e0f
Luxurioust <xuri.me@gmail.com>
2014-12-06 14:43:46 +0800

    add notifications migrate

---
+++ app/database/migrations/2014_12_06_143648_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('notifications', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('sender_id');
+			$table->integer('receiver_id');
+			$table->integer('category');
+			$table->boolean('status')->default('0'); // Read flag
+			$table->timestamp('deleted_at')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('notifications');
+	}
+
+}
