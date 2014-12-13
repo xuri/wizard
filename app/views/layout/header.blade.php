60e434b7405b67196effedb3e351d37b7274b185
Luxurioust <xuri.me@gmail.com>
2014-12-13 21:53:18 +0800

    apple touch icon support update

--- app/views/layout/header.blade.php
+++ app/views/layout/header.blade.php
@@ -9,25 +9,25 @@
 	<meta name="keywords" content="聘爱网,聘爱,Pinai,校内,大学,黑工程,哈商大,哈工大,交友,聊天,精灵,校园" />
 	<meta name="author" content="哈尔滨精灵科技有限责任公司" />
 	{{-- Favicons --}}
-	<link rel="shortcut icon" href="{{ route('home') }}/images/icons/favicon.png" sizes="32x32">
+	<link rel="shortcut icon" href="{{ route('home') }}/assets/images/icons/favicon.png" sizes="32x32">
 	{{-- For Chrome for Android: --}}
-	<link rel="icon" sizes="192x192" href="{{ route('home') }}/images/icons/touch-icon-192x192.png">
+	<link rel="icon" sizes="192x192" href="{{ route('home') }}/assets/images/icons/touch-icon-192x192.png">
 	{{-- For iPhone 6 Plus with @3× display: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="180x180" href="{{ route('home')}}/images/icons/apple-touch-icon-180x180-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="180x180" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-180x180-precomposed.png">
 	{{-- For iPad with @2× display running iOS ≥ 7: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ route('home')}}/images/icons/apple-touch-icon-152x152-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-152x152-precomposed.png">
 	{{-- For iPad with @2× display running iOS ≤ 6: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ route('home')}}/images/icons/apple-touch-icon-144x144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-144x144-precomposed.png">
 	{{-- For iPhone with @2× display running iOS ≥ 7: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ route('home')}}/images/icons/apple-touch-icon-120x120-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-120x120-precomposed.png">
 	{{-- For iPhone with @2× display running iOS ≤ 6: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ route('home')}}/images/icons/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
 	{{-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ route('home')}}/images/icons/apple-touch-icon-76x76-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-76x76-precomposed.png">
 	{{-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6: --}}
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ route('home')}}/images/icons/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
 	{{-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: --}}
-	<link rel="apple-touch-icon-precomposed" href="{{ route('home')}}/images/icons/apple-touch-icon-precomposed.png">{{-- 57×57px --}}
+	<link rel="apple-touch-icon-precomposed" href="{{ route('home') }}/assets/images/icons/apple-touch-icon-precomposed.png">{{-- 57×57px --}}
 	{{-- The Stylesheets --}}
 
 	{{ HTML::style('assets/css/style.css') }}
