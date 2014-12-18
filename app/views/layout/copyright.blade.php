88e80e00489375cc91793292b4cca12957192518
Luxurioust <xuri.me@gmail.com>
2014-12-18 11:18:51 +0800

    add ICP copyright

--- app/views/layout/copyright.blade.php
+++ app/views/layout/copyright.blade.php
@@ -1,3 +1,3 @@
 <div class="footer">
-	<p>Copyright &copy; 2013 - <?php echo date('Y'); ?> All rights reserved. 哈尔滨精灵科技有限责任公司</p>
+	<p>Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="http://www.jinglingkj.com" target="_blank" style="color: #FFF;">哈尔滨精灵科技有限责任公司</a> All rights reserved. <a href="http://www.miitbeian.gov.cn/" target="_blank" style="color: #FFF;">黑ICP备14007294号</a></p>
 </div>
