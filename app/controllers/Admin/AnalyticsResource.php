6315d47398dfb7171c306fb06b0ef2aa3e9e0938
Luxurioust <xuri.me@gmail.com>
2014-12-19 09:55:07 +0800

    charts update

--- app/controllers/Admin/AnalyticsResource.php
+++ app/controllers/Admin/AnalyticsResource.php
@@ -86,6 +86,240 @@ class Admin_AnalyticsResource extends BaseResource
 	 */
 	public function userCharts()
 	{
-		return View::make($this->resourceView.'.user-charts')->with(compact(''));
+
+		$analyticsUser = AnalyticsUser::select(
+							'all_user',
+							'daily_active_user',
+							'weekly_active_user',
+							'monthly_active_user',
+							'all_male_user',
+							'daily_active_male_user',
+							'weekly_active_male_user',
+							'monthly_active_male_user',
+							'all_female_user',
+							'daily_active_female_user',
+							'weekly_active_female_user',
+							'monthly_active_female_user',
+							'complete_profile_user_ratio',
+							'from_web',
+							'from_android',
+							'from_ios',
+							'created_at'
+						)->orderBy('created_at')->take(31)->get()->toArray(); // Retrive analytics data
+
+		/*
+		|--------------------------------------------------------------------------
+		| User Basic Analytics Section
+		|--------------------------------------------------------------------------
+		|
+		*/
+
+		$allUser = array(); // Create all user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$allUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['all_user']);
+		}
+
+		$fromWeb = array(); // Create all from web user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$fromWeb[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['from_web']);
+		}
+
+		$fromAndroid = array(); // Create all from Android user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$fromAndroid[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['from_android']);
+		}
+
+		$fromiOS = array(); // Create all from iOS user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$fromiOS[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['from_ios']);
+		}
+
+		$allMaleUser = array(); // Create all male user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$allMaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['all_male_user']);
+		}
+
+		$allFemaleUser = array(); // Create all female user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$allFemaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['all_female_user']);
+		}
+
+		// Build Json data (remove double quotes from Json return data)
+		$userBasicAnalytics = '{
+			"总用户":'.preg_replace('/["]/', '' ,json_encode($allUser)).
+			', "男用户":'.preg_replace('/["]/', '' ,json_encode($allMaleUser)).
+			', "女用户":'.preg_replace('/["]/', '' ,json_encode($allFemaleUser)).
+			', "Web用户":'.preg_replace('/["]/', '' ,json_encode($fromWeb)).
+			', "Android用户":'.preg_replace('/["]/', '' ,json_encode($fromAndroid)).
+			', "iOS用户":'.preg_replace('/["]/', '' ,json_encode($fromiOS)).
+			'}';
+
+		/*
+		|--------------------------------------------------------------------------
+		| User Daily Active Analytics Section
+		|--------------------------------------------------------------------------
+		|
+		*/
+
+		$dailyActiveUser = array(); // Create daily active user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$dailyActiveUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['daily_active_user']);
+		}
+
+		$dailyActiveMaleUser = array(); // Create daily active male user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$dailyActiveMaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['daily_active_male_user']);
+		}
+
+		$dailyActiveFemaleUser = array(); // Create daily active female user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$dailyActiveFemaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['daily_active_female_user']);
+		}
+
+		// Build Json data (remove double quotes from Json return data)
+		$dailyActiveUserAnalytics = '{
+			"日活用户":'.preg_replace('/["]/', '' ,json_encode($dailyActiveUser)).
+			', "日活男用户":'.preg_replace('/["]/', '' ,json_encode($dailyActiveMaleUser)).
+			', "日活女用户":'.preg_replace('/["]/', '' ,json_encode($dailyActiveFemaleUser)).
+			'}';
+
+		/*
+		|--------------------------------------------------------------------------
+		| User Weekly Active Analytics Section
+		|--------------------------------------------------------------------------
+		|
+		*/
+
+		$weeklyActiveUser = array(); // Create weekly active user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$weeklyActiveUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['weekly_active_user']);
+		}
+
+		$weeklyActiveMaleUser = array(); // Create weekly active male user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$weeklyActiveMaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['weekly_active_male_user']);
+		}
+
+		$weeklyActiveFemaleUser = array(); // Create weekly active female user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$weeklyActiveFemaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['weekly_active_female_user']);
+		}
+
+		// Build Json data (remove double quotes from Json return data)
+		$weeklyActiveUserAnalytics = '{
+			"周活用户":'.preg_replace('/["]/', '' ,json_encode($weeklyActiveUser)).
+			', "周活男用户":'.preg_replace('/["]/', '' ,json_encode($weeklyActiveMaleUser)).
+			', "周活女用户":'.preg_replace('/["]/', '' ,json_encode($weeklyActiveFemaleUser)).
+			'}';
+
+		/*
+		|--------------------------------------------------------------------------
+		| User Monthly Active Analytics Section
+		|--------------------------------------------------------------------------
+		|
+		*/
+
+		$monthlyActiveUser = array(); // Create monthly active user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$monthlyActiveUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['monthly_active_user']);
+		}
+
+		$monthlyActiveMaleUser = array(); // Create monthly active male user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$monthlyActiveMaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['monthly_active_male_user']);
+		}
+
+		$monthlyActiveFemaleUser = array(); // Create monthly active female user array
+		foreach($analyticsUser as $key){ // Structure array elements
+			$monthlyActiveFemaleUser[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['monthly_active_female_user']);
+		}
+
+		// Build Json data (remove double quotes from Json return data)
+		$monthlyActiveUserAnalytics = '{
+			"月活用户":'.preg_replace('/["]/', '' ,json_encode($monthlyActiveUser)).
+			', "月活男用户":'.preg_replace('/["]/', '' ,json_encode($monthlyActiveMaleUser)).
+			', "月活女用户":'.preg_replace('/["]/', '' ,json_encode($monthlyActiveFemaleUser)).
+			'}';
+
+		/*
+		|--------------------------------------------------------------------------
+		| User Profile Complete Ratio Analytics Section
+		|--------------------------------------------------------------------------
+		|
+		*/
+
+		$completeProfileUserRatio = array();
+		foreach($analyticsUser as $key){ // Structure array elements
+			$completeProfileUserRatio[] = array(
+				date('Y', strtotime($key['created_at'])),
+				date('m', strtotime($key['created_at'])),
+				date('d', strtotime($key['created_at'])),
+				$key['complete_profile_user_ratio']);
+		}
+
+		// Build Json data (remove double quotes from Json return data)
+		$completeProfileUserRatioAnalytics = '{"完整用户资料比例":'.preg_replace('/["]/', '' ,json_encode($completeProfileUserRatio)).'}';
+
+		return View::make($this->resourceView.'.user-charts')->with(compact('userBasicAnalytics', 'dailyActiveUserAnalytics', 'weeklyActiveUserAnalytics', 'monthlyActiveUserAnalytics', 'completeProfileUserRatioAnalytics'));
 	}
 }
--- app/views/admin/analytics/user-charts.blade.php
+++ app/views/admin/analytics/user-charts.blade.php
@@ -18,12 +18,12 @@
 				<div class="col-lg-12">
 					<div class="panel panel-default">
 						<div class="panel-heading">
-							用户总数统计
+							用户总数及来源趋势图
 						</div>
 						<!-- /.panel-heading -->
 						<div class="panel-body">
 							<div class="flot-chart">
-								<div class="flot-chart-content" id="flot-line-chart2"></div>
+								<div class="flot-chart-content" id="flot-line-chart-1"></div>
 							</div>
 						</div>
 						<!-- /.panel-body -->
@@ -34,12 +34,57 @@
 				<div class="col-lg-12">
 					<div class="panel panel-default">
 						<div class="panel-heading">
-							用户总数统计
+							用户日活动状况趋势图
 						</div>
 						<!-- /.panel-heading -->
 						<div class="panel-body">
 							<div class="flot-chart">
-								<div class="flot-chart-content" id="flot-line-chart"></div>
+								<div class="flot-chart-content" id="flot-line-chart-2"></div>
+							</div>
+						</div>
+						<!-- /.panel-body -->
+					</div>
+					<!-- /.panel -->
+				</div>
+				<div class="col-lg-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							用户周活动状况趋势图
+						</div>
+						<!-- /.panel-heading -->
+						<div class="panel-body">
+							<div class="flot-chart">
+								<div class="flot-chart-content" id="flot-line-chart-3"></div>
+							</div>
+						</div>
+						<!-- /.panel-body -->
+					</div>
+					<!-- /.panel -->
+				</div>
+				<div class="col-lg-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							用户月活动状况趋势图
+						</div>
+						<!-- /.panel-heading -->
+						<div class="panel-body">
+							<div class="flot-chart">
+								<div class="flot-chart-content" id="flot-line-chart-4"></div>
+							</div>
+						</div>
+						<!-- /.panel-body -->
+					</div>
+					<!-- /.panel -->
+				</div>
+				<div class="col-lg-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							用户月活动状况趋势图
+						</div>
+						<!-- /.panel-heading -->
+						<div class="panel-body">
+							<div class="flot-chart">
+								<div class="flot-chart-content" id="flot-line-chart-5"></div>
 							</div>
 						</div>
 						<!-- /.panel-body -->
@@ -47,85 +92,6 @@
 					<!-- /.panel -->
 				</div>
 				<!-- /.col-lg-12 -->
-				<div class="col-lg-6">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							Pie Chart Example
-						</div>
-						<!-- /.panel-heading -->
-						<div class="panel-body">
-							<div class="flot-chart">
-								<div class="flot-chart-content" id="flot-pie-chart"></div>
-							</div>
-						</div>
-						<!-- /.panel-body -->
-					</div>
-					<!-- /.panel -->
-				</div>
-				<!-- /.col-lg-6 -->
-				<div class="col-lg-6">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							Multiple Axes Line Chart Example
-						</div>
-						<!-- /.panel-heading -->
-						<div class="panel-body">
-							<div class="flot-chart">
-								<div class="flot-chart-content" id="flot-line-chart-multi"></div>
-							</div>
-						</div>
-						<!-- /.panel-body -->
-					</div>
-					<!-- /.panel -->
-				</div>
-				<!-- /.col-lg-6 -->
-				<div class="col-lg-6">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							Moving Line Chart Example
-						</div>
-						<!-- /.panel-heading -->
-						<div class="panel-body">
-							<div class="flot-chart">
-								<div class="flot-chart-content" id="flot-line-chart-moving"></div>
-							</div>
-						</div>
-						<!-- /.panel-body -->
-					</div>
-					<!-- /.panel -->
-				</div>
-				<!-- /.col-lg-6 -->
-				<div class="col-lg-6">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							Bar Chart Example
-						</div>
-						<!-- /.panel-heading -->
-						<div class="panel-body">
-							<div class="flot-chart">
-								<div class="flot-chart-content" id="flot-bar-chart"></div>
-							</div>
-						</div>
-						<!-- /.panel-body -->
-					</div>
-					<!-- /.panel -->
-				</div>
-				<!-- /.col-lg-6 -->
-				<div class="col-lg-12">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							Flot Charts Usage
-						</div>
-						<!-- /.panel-heading -->
-						<div class="panel-body">
-							<p>Flot is a pure JavaScript plotting library for jQuery, with a focus on simple usage, attractive looks, and interactive features. In SB Admin, we are using the most recent version of Flot along with a few plugins to enhance the user experience. The Flot plugins being used are the tooltip plugin for hoverable tooltips, and the resize plugin for fully responsive charts. The documentation for Flot Charts is available on their website, <a target="_blank" href="http://www.flotcharts.org/">http://www.flotcharts.org/</a>.</p>
-							<a target="_blank" class="btn btn-default btn-lg btn-block" href="http://www.flotcharts.org/">View Flot Charts Documentation</a>
-						</div>
-						<!-- /.panel-body -->
-					</div>
-					<!-- /.panel -->
-				</div>
-				<!-- /.col-lg-6 -->
 			</div>
 			<!-- /.row -->
 		</div>
@@ -146,13 +112,238 @@
 	{{-- HTML::script('assets/js/chartjs/Chart.min.js') --}}
 
 	{{-- Flot Charts JavaScript --}}
-    {{ HTML::script('assets/js/admin/plugins/flot/excanvas.min.js') }}
-    {{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.js') }}
-    {{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.pie.js') }}
-    {{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.resize.js') }}
-    {{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.tooltip.min.js') }}
-    {{ HTML::script('assets/js/admin/plugins/flot/flot-data.js') }}
+	{{ HTML::script('assets/js/admin/plugins/flot/excanvas.min.js') }}
+	{{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.js') }}
+	{{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.pie.js') }}
+	{{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.resize.js') }}
+	{{ HTML::script('assets/js/admin/plugins/flot/jquery.flot.tooltip.min.js') }}
+	{{-- HTML::script('assets/js/admin/plugins/flot/flot-data.js') --}}
 
+	<script>
+	{{-- User Basic Analytics Section --}}
+	$(function() {
+		var offset = 0;
+		plot();
+
+		function plot() {
+			var obj = {{ $userBasicAnalytics }};
+
+			seriesData = [];
+
+			for (var prop in obj) {
+				seriesData.push({label: prop, data:$.map(obj[prop], function(i,j){
+					 return [[new Date(i[0],i[1]-1, i[2]-1).getTime(), i[3]]];
+				})});
+			}
+
+			var options = {
+				series: {
+					shadowSize: 4, {{-- Line shadow --}}
+					lines: {
+						show: true
+					},
+					points: {
+						show: true
+					}
+				},
+				grid: {
+					hoverable: true {{-- IMPORTANT! this is needed for tooltip to work --}}
+				},
+				xaxis: { mode: "time", timeformat: "%y-%m-%d" },
+				tooltip: true,
+				tooltipOpts: {
+					content: "截止 %x.1 %s： %y.4",
+					shifts: {
+						x: -60,
+						y: 25
+					}
+				}
+			};
+
+			var plotObj = $.plot($("#flot-line-chart-1"), seriesData, options);
+		}
+	});
+
+	{{-- User Daily Active Analytics Section --}}
+
+	$(function() {
+		var offset = 0;
+		plot();
+
+		function plot() {
+			var obj = {{ $dailyActiveUserAnalytics }};
+
+			seriesData = [];
+
+			for (var prop in obj) {
+				seriesData.push({label: prop, data:$.map(obj[prop], function(i,j){
+					 return [[new Date(i[0],i[1]-1, i[2]-1).getTime(), i[3]]];
+				})});
+			}
+
+			var options = {
+				series: {
+					shadowSize: 4, {{-- Line shadow --}}
+					lines: {
+						show: true
+					},
+					points: {
+						show: true
+					}
+				},
+				grid: {
+					hoverable: true {{-- IMPORTANT! this is needed for tooltip to work --}}
+				},
+				xaxis: { mode: "time", timeformat: "%y-%m-%d" },
+				tooltip: true,
+				tooltipOpts: {
+					content: "%x.1 %s： %y.4",
+					shifts: {
+						x: -60,
+						y: 25
+					}
+				}
+			};
+
+			var plotObj = $.plot($("#flot-line-chart-2"), seriesData, options);
+		}
+	});
+
+	{{-- User Weekly Active Analytics Section --}}
+
+	$(function() {
+		var offset = 0;
+		plot();
+
+		function plot() {
+			var obj = {{ $weeklyActiveUserAnalytics }};
+
+			seriesData = [];
+
+			for (var prop in obj) {
+				seriesData.push({label: prop, data:$.map(obj[prop], function(i,j){
+					 return [[new Date(i[0],i[1]-1, i[2]-1).getTime(), i[3]]];
+				})});
+			}
+
+			var options = {
+				series: {
+					shadowSize: 4, {{-- Line shadow --}}
+					lines: {
+						show: true
+					},
+					points: {
+						show: true
+					}
+				},
+				grid: {
+					hoverable: true {{-- IMPORTANT! this is needed for tooltip to work --}}
+				},
+				xaxis: { mode: "time", timeformat: "%y-%m-%d" },
+				tooltip: true,
+				tooltipOpts: {
+					content: "%x.1 %s： %y.4",
+					shifts: {
+						x: -60,
+						y: 25
+					}
+				}
+			};
+
+			var plotObj = $.plot($("#flot-line-chart-3"), seriesData, options);
+		}
+	});
+
+	{{-- User Monthly Active Analytics Section --}}
+
+	$(function() {
+		var offset = 0;
+		plot();
+
+		function plot() {
+			var obj = {{ $monthlyActiveUserAnalytics }};
+
+			seriesData = [];
+
+			for (var prop in obj) {
+				seriesData.push({label: prop, data:$.map(obj[prop], function(i,j){
+					 return [[new Date(i[0],i[1]-1, i[2]-1).getTime(), i[3]]];
+				})});
+			}
+
+			var options = {
+				series: {
+					shadowSize: 4, {{-- Line shadow --}}
+					lines: {
+						show: true
+					},
+					points: {
+						show: true
+					}
+				},
+				grid: {
+					hoverable: true {{-- IMPORTANT! this is needed for tooltip to work --}}
+				},
+				xaxis: { mode: "time", timeformat: "%y-%m-%d" },
+				tooltip: true,
+				tooltipOpts: {
+					content: "%x.1 %s： %y.4",
+					shifts: {
+						x: -60,
+						y: 25
+					}
+				}
+			};
+
+			var plotObj = $.plot($("#flot-line-chart-4"), seriesData, options);
+		}
+	});
+
+	{{-- User Profile Complete Ratio Analytics Section --}}
+
+	$(function() {
+		var offset = 0;
+		plot();
+
+		function plot() {
+			var obj = {{ $completeProfileUserRatioAnalytics }};
+
+			seriesData = [];
+
+			for (var prop in obj) {
+				seriesData.push({label: prop, data:$.map(obj[prop], function(i,j){
+					 return [[new Date(i[0],i[1]-1, i[2]-1).getTime(), i[3]]];
+				})});
+			}
+
+			var options = {
+				series: {
+					shadowSize: 4, {{-- Line shadow --}}
+					lines: {
+						show: true
+					},
+					points: {
+						show: true
+					}
+				},
+				grid: {
+					hoverable: true {{-- IMPORTANT! this is needed for tooltip to work --}}
+				},
+				xaxis: { mode: "time", timeformat: "%y-%m-%d" },
+				tooltip: true,
+				tooltipOpts: {
+					content: "%x.1 %s： %y.4 %",
+					shifts: {
+						x: -60,
+						y: 25
+					}
+				}
+			};
+
+			var plotObj = $.plot($("#flot-line-chart-5"), seriesData, options);
+		}
+	});
+	</script>
 	{{-- Custom Theme JavaScript --}}
 	{{ HTML::script('assets/js/admin/admin.js') }}
 
