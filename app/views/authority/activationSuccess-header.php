475ce0d8486d6347213c6c2ac75be1a33fb25a7f
Luxurioust <xuri.me@gmail.com>
2015-01-12 09:41:01 +0800

    update #2

--- app/views/authority/activationSuccess-header.php
+++ app/views/authority/activationSuccess-header.php
@@ -6,11 +6,11 @@
 	@include('layout.meta')
 	@yield('content')
 
-	<meta content="3; {{ route('account') }}" http-equiv="refresh">
-
 	{{ HTML::style('assets/css/reset.css') }}
 	{{ HTML::style('assets/css/nav.css') }}
 	{{ HTML::style('assets/css/signin.css') }}
 	{{ HTML::style('assets/font-awesome-4.2.0/css/font-awesome.min.css') }}
+
+	<meta content="4; {{ route('account') }}" http-equiv="refresh">
 </head>
 <body>
