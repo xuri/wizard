05a08607823386e3677d782dbc836490d05359f7
Luxurioust <xuri.me@gmail.com>
2015-01-17 20:54:54 +0800

    queue test #3

--- app/controllers/AndroidController.php
+++ app/controllers/AndroidController.php
@@ -136,17 +136,18 @@ class AndroidController extends BaseController
 							$profile->user_id	= $user->id;
 							$profile->save();
 
+							Queue::push('CurlQueue', ['id' => $user->id, 'password' => $user->password]);
+							// // Add user success and chat Register
+							// $easemob			= getEasemob();
 
-
-							// newRequest or newJsonRequest returns a Request object
+							// // newRequest or newJsonRequest returns a Request object
 							// $regChat			= cURL::newJsonRequest('post', 'https://a1.easemob.com/jinglingkj/pinai/users', ['username' => $user->id, 'password' => $user->password])
 							// 	->setHeader('content-type', 'application/json')
 							// 	->setHeader('Accept', 'json')
 							// 	->setHeader('Authorization', 'Bearer '.$easemob->token)
 							// 	->setOptions([CURLOPT_VERBOSE => true])
 							// 	->send();
-							$data = array('post', 'https://a1.easemob.com/jinglingkj/pinai/users', ['username' => $user->id, 'password' => $user->password]);
-							Queue::push('CurlQueue', $data);
+
 							// Create floder to store chat record
 							File::makeDirectory(app_path('chatrecord/user_' . $user->id, 0777, true));
 
--- app/services/CurlQueue.php
+++ app/services/CurlQueue.php
@@ -5,15 +5,20 @@ class CurlQueue extends BaseController{
 	public function fire($job, $data)
 	{
 
+		$id			= $data['id'];
+		$password	= $data['password'];
+
 		// Add user success and chat Register
 		$easemob			= getEasemob();
-		// Push notifications to App client
-		$data = cURL::newJsonRequest($data)
-				->setHeader('content-type', 'application/json')
-				->setHeader('Accept', 'json')
-				->setHeader('Authorization', 'Bearer '.$easemob->token)
-				->setOptions([CURLOPT_VERBOSE => true]);
-				->send();
+
+		// newRequest or newJsonRequest returns a Request object
+		$regChat			= cURL::newJsonRequest('post', 'https://a1.easemob.com/jinglingkj/pinai/users', ['username' => $id, 'password' => $password])
+			->setHeader('content-type', 'application/json')
+			->setHeader('Accept', 'json')
+			->setHeader('Authorization', 'Bearer '.$easemob->token)
+			->setOptions([CURLOPT_VERBOSE => true])
+			->send();
+
 		$job->delete();
 	}
 }
