8899398bdc25eeb761d51544a2d5fe6e7f727913
Luxurioust <xuri.me@gmail.com>
2015-01-24 15:17:43 +0800

    analytics code

--- app/views/layout/copyright.blade.php
+++ app/views/layout/copyright.blade.php
@@ -1,3 +1,8 @@
 <div class="footer">
 	Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="http://www.jinglingkj.com" target="_blank">哈尔滨精灵科技有限责任公司</a> All rights reserved. <a href="http://www.miitbeian.gov.cn/" target="_blank">黑ICP备14007294号</a>
 </div>
+
+@include('layout.analytics')
+@yield('content')
+
+<?php echo '<img src="'._cnzzTrackPageView(5808356).'" width="0" height="0"/>';?>
