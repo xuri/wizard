babacc1974bdabf08ef963dfb9859a528eeea34c
Luxurioust <xuri.me@gmail.com>
2015-03-03 21:01:07 +0800

    fix analytics

--- app/views/layout/analytics.php
+++ app/views/layout/analytics.php
@@ -1,1 +1,1 @@
-<?php require_once 'cs.php';echo '<img style="display: none" src="'._cnzzTrackPageView(1254405189).'" width="0" height="0"/>';?>
+<?php require_once 'cs.php';echo '<img style="display: none" src="'._cnzzTrackPageView(5808356).'" width="0" height="0"/>';?>
