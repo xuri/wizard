f5f11002fd1d2c0193ac32eeb8bffd9c64b94f9d
Luxurioust <xuri.me@gmail.com>
2015-04-29 18:00:30 +0800

    Wechat share support

--- app/views/wap/index.blade.php
+++ app/views/wap/index.blade.php
@@ -1,3 +1,9 @@
+<?php
+include_once( app_path('api/wechat/jssdk.php') );
+$jssdk			= new JSSDK("wx85a303018cc9100b", "be2909ec1f4f590feb25aa6638a63d5f");
+$signPackage	= $jssdk->GetSignPackage();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,7 +20,7 @@ li{ list-style:none; }
 body{
 	background:#fe949e;
 	font-size:8px;
-    font-family:Microsoft YaHei,SimHei,Arial,Pro LiHei Pro Medium;
+	font-family:Microsoft YaHei,SimHei,Arial,Pro LiHei Pro Medium;
 }
 .top {
 	width: 100%;
@@ -51,6 +57,41 @@ body{
 	<div class="bottom">
 		<a href="{{ route('wap.members') }}/?sex=F">{{ HTML::image('assets/images/wap/female.png', '', array('class' => 'sex_btn')) }}</a>
 	</div>
+
+	<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+	<script type="text/javascript">
+	  wx.config({
+		debug: false,
+		appId: '<?php echo $signPackage["appId"];?>',
+		timestamp: <?php echo $signPackage["timestamp"];?>,
+		nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+		signature: '<?php echo $signPackage["signature"];?>',
+		jsApiList: [
+		  // 所有要调用的 API 都要加到这个列表中
+		'onMenuShareTimeline'
+		]
+	  });
+
+	 wx.ready(function () {
+			// 在这里调用 API
+			wx.onMenuShareTimeline({
+			title: '聘爱丨全国首个大学生恋爱APP', // 分享标题
+			link: 'http://www.pinai521.com/wap', // 分享链接
+			imgUrl: "http://www.pinai521.com/assets/images/wechat/boy.jpg", // 分享图标
+			success: function () {
+			},
+			cancel: function () {
+			},
+			fail: function (res) {
+			alert('wx.onMenuShareTimeline:fail: '+JSON.stringify(res));
+			}
+			});
+	  });
+	 wx.error(function (res) {
+			alert('wx.error: '+JSON.stringify(res));
+	  });
+	</script>
+
 	@include('layout.analytics')
 	@yield('content')
 </body>
