60bdb9bae8db3f7aa06910db6d35f39c36078e72
Luxurioust <xuri.me@gmail.com>
2015-05-14 13:08:40 +0800

    update support and feedback API

--- app/controllers/AndroidController.php
+++ app/controllers/AndroidController.php
@@ -3206,22 +3206,61 @@ class AndroidController extends BaseController
 
 				// Support
 				case 'support' :
-					$support			= new Support;
-					$support->user_id 	= Input::get('id');
-					$support->content	= htmlentities(Input::get('content'));
-					if($support->save()) {
+
+					// Get feedback user id
+					$user_id			= Input::get('id');
+
+					// Format feedback content
+					$feedback			= app_input_filter(Input::get('content'));
+
+					// Check feedback exist
+					$feedback_exist 	= Support::where('user_id', $user_id)
+											->where('content', $feedback)
+											->where('created_at', '>=', Carbon::today())
+											->count();
+
+					// User already send feedback today
+					if($feedback_exist >= 1) {
 						return Response::json(
 							array(
-								'status' 		=> 1
+								'status' 			=> 0,
+								'empty'				=> 0,
+								'repeat'			=> 1
 							)
 						);
 					} else {
-						return Response::json(
-							array(
-								'status' 		=> 0
-							)
-						);
+						if($feedback != "") {
+							$support			= new Support;
+							$support->user_id 	= $user_id;
+							$support->content	= $feedback;
+							if($support->save()) {
+								return Response::json(
+									array(
+										'status' 		=> 1,
+										'empty'			=> 0,
+										'repeat'		=> 0
+									)
+								);
+							} else {
+								return Response::json(
+									array(
+										'status' 		=> 0,
+										'empty'			=> 0,
+										'repeat'		=> 0
+									)
+								);
+							}
+						} else {
+							return Response::json(
+								array(
+									'status' 			=> 0,
+									'empty'				=> 1,
+									'repeat'			=> 0
+								)
+							);
+						}
 					}
+
 				break;
 
 				// Admin notifications
