d2ec60a3b7136a005667519ab30511b3996a48ea
Luxurioust <xuri.me@gmail.com>
2015-06-14 10:40:10 +0800

    update match_user return value bugs in API

--- app/models/User.php
+++ app/models/User.php
@@ -17,8 +17,9 @@
 /**
  * User
  */
-
+use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
