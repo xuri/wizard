d50a464e10f827068359e603d29939a5694364e7
Luxurioust <xuri.me@gmail.com>
2015-07-13 17:34:10 +0800

    update WAP style

--- app/views/wap/redirect.blade.php
+++ app/views/wap/redirect.blade.php
@@ -6,18 +6,33 @@
     {{ HTML::script('assets/js/jquery-1.11.1/jquery.min.js') }}
     <title>点击下载应用</title>
     <style type="text/css">
-    *{margin:0; padding:0;}
-    a{text-decoration: none;}
-    img{max-width: 100%; height: auto;}
-    .weixin-tip{display: none; position: fixed; left:0; top:0; bottom:0; background: rgba(0,0,0,0.8); filter:alpha(opacity=80);  height: 100%; width: 100%; z-index: 100;}
-    .weixin-tip p{text-align: center; margin-top: 10%; padding:0 5%;}
+        body,h1,h2,h3,h4,h5,h6,p,dl,dd,ul,ol,pre,form,input,textarea,th,td,select{margin:0; padding:0;}
+        em{font-style:normal}
+        li{list-style:none}
+        a{text-decoration:none;}
+        img{border:none; vertical-align:top;}
+        table{border-collapse:collapse;}
+        input,textarea{outline:none;}
+        textarea{ resize:none; overflow:auto;}
+        body{font-size:12px;font-family:Microsoft YaHei,SimHei,Arial,Pro LiHei Pro Medium;padding-bottom: 50px;
+    background:#fe949e;content:center;}
+        .div_t{width:100%;height:100px;}
+        .btn{height:60px;float:right;margin-top:20px;margin-right:20px;}
+        .tip{color:#fff;font-size:16px;float:right;margin-top:70px;margin-right:10px;}
+        .logo{margin:0 auto;}
+        .div_b{width:150px;margin:100px auto 0;text-align:center;}
+        .logo{width:150px;margin-bottom:15px;}
+        .tip_b{color:#fff;font-size:15px;margin-top:20px;}
     </style>
 </head>
 <body>
-    <div class="weixin-tip">
-        <p>
-            {{ HTML::image('assets/images/wap/live_weixin.png', '微信打开', array()) }}
-        </p>
+    <div class="div_t">
+    {{ HTML::image('assets/images/wap/wx_btn.png', '微信打开', array('class' => 'btn')) }}
+    <span class="tip">请点击从浏览器打开</span>
+    </div>
+    <div class="div_b">
+        {{ HTML::image('assets/images/wap/wx_tip.png', '微信打开', array('class' => 'logo')) }}
+        <span class="tip_b">浏览器不支持下载</span>
     </div>
     <script type="text/javascript">
         $(window).on("load",function(){
