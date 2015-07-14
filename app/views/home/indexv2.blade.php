82abef5a5dc6790b99e2e5fba2789409fcb9b87a
Luxurioust <xuri.me@gmail.com>
2015-07-14 10:57:49 +0800

    update set language

--- app/views/home/indexv2.blade.php
+++ app/views/home/indexv2.blade.php
@@ -188,10 +188,10 @@
         <div class="col-sm-8 col-sm-offset-2">
             <p class="copy">Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="http://www.jinglingkj.com" target="_blank">{{ Lang::get('footer.company') }}</a> All rights reserved. {{ Lang::get('footer.icp_license') }} <a href="http://www.miitbeian.gov.cn/" target="_blank">黑ICP备14007294号</a>
                 <br />
-                <a href="{{ route('home') }}/?lang=zh" title="简体中文" alt="简体中文">
+                <a href="javascript:void(0);" title="简体中文" alt="简体中文" class="set_lang_zh">
                     {{ HTML::image('assets/images/china_flag.svg', '', array('style' => 'max-width:100%; margin: 0 0 0.2em 1em;', 'width' => '22', 'height' => '15')) }}
                 </a>
-                <a href="{{ route('home') }}/?lang=en" title="English" alt="English">
+                <a href="javascript:void(0);" title="English" alt="English" class="set_lang_en">
                     {{ HTML::image('assets/images/us_flag.svg', '', array('style' => 'max-width:100%; margin: 0 0 0.2em 0.5em;', 'width' => '22', 'height' => '15')) }}
                 </a>
             </p>
@@ -199,7 +199,9 @@
     </footer>
 
     <script>
-        var appstore = "{{ Lang::get('index.appstore') }}";
+        var appstore  = "{{ Lang::get('index.appstore') }}";
+        var csrfToken = "{{ csrf_token() }}";
+        var homeRoute = "{{ route('home') }}";
     </script>
     {{-- jQuery --}}
     {{ HTML::script('assets/js/jquery-1.11.1/jquery.min.js') }}
@@ -215,6 +217,20 @@
         '/assets/js/appear/jquery.appear.js',
         '/assets/js/mainv2.js'
     )) }}
+
+    <script>
+
+        $('.set_lang_zh').click(function(e) {
+            $.post(homeRoute, { _token: csrfToken, lang: "zh" } );
+            e.preventDefault();
+        });
+
+        $('.set_lang_en').click(function(e) {
+            $.post(homeRoute, { _token: csrfToken, lang: "en" } );
+            e.preventDefault();
+        });
+
+    </script>
 
     {{-- Analytics Code --}}
     @include('layout.analytics')
--- app/views/layout/copyright.blade.php
+++ app/views/layout/copyright.blade.php
@@ -1,18 +1,28 @@
 <div class="footer">
     Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="{{ route('home') }}" target="_blank">{{ Lang::get('footer.company') }}</a> All rights reserved. {{ Lang::get('footer.icp_license') }} <a href="http://www.miitbeian.gov.cn/" target="_blank">黑ICP备14007294号</a>
-    {{ Form::open(array('method' => 'post', 'route' => 'home')) }}
-    <input type="hidden" name="lang" value="zh" />
-    <button type="submit" title="简体中文" alt="简体中文">
+    <a href="javascript:void(0);" title="简体中文" alt="简体中文" class="set_lang_zh">
         {{ HTML::image('assets/images/china_flag.svg', '', array('style' => 'max-width:100%; margin: 0 0 0.2em 1em;', 'width' => '22', 'height' => '15')) }}
-    </button>
-    {{ Form::close() }}
-    {{ Form::open(array('method' => 'post', 'route' => 'home')) }}
-    <input type="hidden" name="lang" value="en" />
-    <button type="submit"  title="English" alt="English">
+    </a>
+    <a href="javascript:void(0);" title="English" alt="English" class="set_lang_en">
         {{ HTML::image('assets/images/us_flag.svg', '', array('style' => 'max-width:100%; margin: 0 0 0.2em 0.5em;', 'width' => '22', 'height' => '15')) }}
-    </button>
-    {{ Form::close() }}
+    </a>
 </div>
+<script>
+
+    var csrfToken = '{{ csrf_token() }}';
+    var homeRoute = '{{ route("home") }}';
+
+    $('.set_lang_zh').click(function(e) {
+        $.post(homeRoute, { _token: csrfToken, lang: "zh" } );
+        e.preventDefault();
+    });
+
+    $('.set_lang_en').click(function(e) {
+        $.post(homeRoute, { _token: csrfToken, lang: "en" } );
+        e.preventDefault();
+    });
+
+</script>
 
 @include('layout.analytics')
 @yield('content')
